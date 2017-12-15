9318574b2280d9e00110493156971b64098f3edd
izavits <izavits@gmail.com>
2017-12-15 15:49:10 +0200

    Add index js main script

---
+++ index.js
@@ -0,0 +1,13 @@
+// Export the core analytics lib and the integrations
+
+var core = require('./lib/core/index.js');
+var mixpanelIntegration = require('./lib/integrations/mixpanel-node/index.js');
+var sentryIntegration = require('./lib/integrations/sentry-node/index.js');
+var gosquaredIntegration = require('./lib/integrations/gosquared-node/index.js');
+
+module.exports = {
+    core: core,
+    mixpanelIntegration: mixpanelIntegration,
+    sentryIntegration: sentryIntegration,
+    gosquaredIntegration: gosquaredIntegration
+};
